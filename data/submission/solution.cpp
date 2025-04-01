cc9e8aa735daa87f30d3755fe0706f485f871141
parthokr <partho.kr@proton.me>
2025-04-01 17:03:33 +0600

    Add cpp solution

--- data/submission/solution.cpp
+++ data/submission/solution.cpp
@@ -3,12 +3,14 @@
 
 using namespace std;
 int main() {
-int t;
-cin >> t;
-while(t--) {
-    int n;
-    cin >>n;
-    if (n&1) cout << "YES" << endl;
-    else cout << "NO" << endl;
-}
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        if (n & 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
 }
